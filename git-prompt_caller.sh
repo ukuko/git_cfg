78ebf9cbd5b2873415ab92c85bf88b6a6846a05c
Pablo Kurimoto (Linux Mint) <pablo.kurimoto@alstomgroup.com>
2023-08-01 21:54:00 +0900

    fix branch information

--- git-prompt_caller.sh
+++ git-prompt_caller.sh
@@ -7,6 +7,28 @@ export GIT_PS1_SHOWUPSTREAM="auto"
 #PROMPT_COMMAND='__git_ps1 "\u@\h:\w" "\\\$ "'
 
 
-PS1='[\u@\h \W$(__git_ps1 " (%s)")]\$ '
+# https://tldp.org/HOWTO/Bash-Prompt-HOWTO/x329.html
+
+RED="\[\033[1;31m\]"
+GREEN="\[\033[0;32m\]"
+YELLOW="\[\033[1;33m\]"
+BLUE="\[\033[1;34m\]"
+MAGENTA="\[\033[1;35m\]"
+CYAN="\[\033[1;36m\]"
+WHITE="\[\033[0;37m\]"
+THE_END="\[\033[0m\]" # This is needed at the end... 
+
+
+PS1="${debian_chroot:+($debian_chroot)}$GREEN\u$WHITE@$RED\h$WHITE:$YELLOW\w $CYAN$(__git_ps1)$THE_END\$ "
+
+
+
+# PS1='[\u@\h \W$(__git_ps1 " (%s)")]\$ '
 # use existing PS1 settings
 #PROMPT_COMMAND=$(sed -r 's|^(.+)(\\\$\s*)$|__git_ps1 "\1" "\2"|' <<< $PS1)  
+
+#unset color_prompt force_color_prompt
+
+
+# Clean up after ourselves...
+unset RED GREEN YELLOW BLUE MAGENTA CYAN WHITE END
