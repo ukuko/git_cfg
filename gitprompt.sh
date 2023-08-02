bced623bd909e90571642c2b15ed3ae031e322e3
Pablo Kurimoto (Linux Mint) <pablo.kurimoto@alstomgroup.com>
2023-08-02 15:54:19 +0900

    git addjust message

--- gitprompt.sh
+++ gitprompt.sh
@@ -29,9 +29,9 @@ THE_END="\[\033[0m\]" # This is needed at the end...
 if [ "$color_prompt" = yes ]; then
     # PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
     #PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w $(parse_git_branch) \[\033[00m\]\$ '
-    PS1="${debian_chroot:+($debian_chroot)}$GREEN\u$WHITE@$RED\h$WHITE:$YELLOW\w$CYAN$(parse_git_branch)$THE_END\$"  
+    export PS1="${debian_chroot:+($debian_chroot)}$GREEN\u$WHITE@$RED\h$WHITE:$YELLOW\w$CYAN$(parse_git_branch)$THE_END\$"  
 else
-    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+    export PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$'
 fi
 unset color_prompt force_color_prompt
 
