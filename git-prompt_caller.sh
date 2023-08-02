e7ed953a0ec700a6e6f4681857fd806da93887db
Pablo Kurimoto (Linux Mint) <pablo.kurimoto@alstomgroup.com>
2023-08-02 14:20:29 +0900

    adjust message

--- git-prompt_caller.sh
+++ git-prompt_caller.sh
@@ -1,4 +1,22 @@
 # https://tldp.org/HOWTO/Bash-Prompt-HOWTO/x329.html
+#############
+force_color_prompt=yes
+
+if [ -n "$force_color_prompt" ]; then
+    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+	# We have color support; assume it's compliant with Ecma-48
+	# (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+	# a case would tend to support setf rather than setaf.)
+	color_prompt=yes
+    else
+	color_prompt=
+    fi
+fi
+
+
+parse_git_branch() {
+ git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+}
 
 RED="\[\033[1;31m\]"
 GREEN="\[\033[0;32m\]"
@@ -7,20 +25,20 @@ BLUE="\[\033[1;34m\]"
 MAGENTA="\[\033[1;35m\]"
 CYAN="\[\033[1;36m\]"
 WHITE="\[\033[0;37m\]"
-THE_END="\[\033[0m\]" # This is needed at the end... 
+THE_END="\[\033[0m\]" # This is needed at the end...
 
 
-#PS1="${debian_chroot:+($debian_chroot)}$GREEN\u$WHITE@$RED\h$WHITE:$YELLOW\w $CYAN$(__git_ps1)$THE_END\$ "
 
-export PS1="$GREEN \u$WHITE@$RED\h$WHITE:$YELLOW\w$CYAN$(parse_git_branch)$THE_END\$ "
-#PS1="$GREEN \u$WHITE@$RED\h$WHITE:$YELLOW\W$CYAN$(__git_ps1 ' (%s)')$THE_END\$ "
-
-#PS1='[\u@\h \W$(__git_ps1 " (%s)")]\$ '
-# use existing PS1 settings
-#PROMPT_COMMAND=$(sed -r 's|^(.+)(\\\$\s*)$|__git_ps1 "\1" "\2"|' <<< $PS1)  
-
-#unset color_prompt force_color_prompt
-
+if [ "$color_prompt" = yes ]; then
+    # PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+    #PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w $(parse_git_branch) \[\033[00m\]\$ '
+    PS1="$GREEN \u$WHITE@$RED\h$WHITE:$YELLOW\w$CYAN$(parse_git_branch)$THE_END\$ "
+else
+    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+fi
+unset color_prompt force_color_prompt
 
 # Clean up after ourselves...
 unset RED GREEN YELLOW BLUE MAGENTA CYAN WHITE END
+
+#############           
